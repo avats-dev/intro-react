971911e5c2c10db92d018806556083b5a0fc99d3
Aditya Vats <avats-dev@users.noreply.github.com>
2020-08-03 12:05:53 +0530

    render students

--- src/App.jsx
+++ src/App.jsx
@@ -67,22 +67,22 @@ class App extends React.Component {
 
     /* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
+    if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
-          placeholder="Add Assignment..."
-          currList={this.state.assignments}
-          addFunction={this.addAssignment}
+          placeholder="Add student..."
+          currList={this.state.students}
+          addFunction={this.addStudent}
           title="Student Roster"
         />
       );
-    }*/
+    }
 
     /* Uncomment lines below to render grades*/
     /*if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
-          tableNames={this.state.assignments}
+          tableNames={this.state.grades}
           rows={this.state.students}
           addFunction={this.addGrade}
           data={this.state.grades}
